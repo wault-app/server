9d6cf8c4f49f01f7f109806c660a95fd6f0c42b3
Gál Péter <pepyta118@gmail.com>
2021-08-05 14:31:52 +0200

    feat: enable cors for wault.app domain

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import * as helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "https://wault.app",
+    optionsSuccessStatus: 200,
+  });
   app.use(helmet());
   await app.listen(3000);
 }
